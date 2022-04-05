fad7e3cde3af48f9656306745e1819bbbd570759
chrissykrissy <jj2174@nyu.edu>
2022-04-04 21:01:56 -0400

    first commit

---
+++ src/test/java/com/example/jooleproject/JooleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jooleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
